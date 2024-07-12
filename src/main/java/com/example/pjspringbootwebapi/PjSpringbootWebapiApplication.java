3c00717feaccb343b31d040ad6d79658fb1371c1
Haidong Xu <hai_designer@hotmail.com>
2024-07-12 22:59:26 +0200

    add readme

--- src/main/java/com/example/pjspringbootwebapi/PjSpringbootWebapiApplication.java
+++ src/main/java/com/example/pjspringbootwebapi/PjSpringbootWebapiApplication.java
@@ -9,5 +9,5 @@ public class PjSpringbootWebapiApplication {
     public static void main(String[] args) {
         SpringApplication.run(PjSpringbootWebapiApplication.class, args);
     }
-
+    //test
 }
