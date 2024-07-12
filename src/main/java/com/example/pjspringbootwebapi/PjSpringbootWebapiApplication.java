26eedf783af4ee8a9a58392ce9fa4a786cd19a44
Haidong Xu <hai_designer@hotmail.com>
2024-07-12 22:53:26 +0200

    maint;first commit;

---
+++ src/main/java/com/example/pjspringbootwebapi/PjSpringbootWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pjspringbootwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjSpringbootWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjSpringbootWebapiApplication.class, args);
+    }
+
+}
